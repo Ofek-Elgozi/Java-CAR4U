2ec5bef18ac5da313fbc169a0e66b06e2e87edaf
Ofek <ofekel@ac.sce.ac.il>
2021-10-27 21:03:14 +0300

    Program With 3 Activities

--- app/src/main/java/com/example/car4u/Activity3.java
+++ app/src/main/java/com/example/car4u/Activity3.java
@@ -2,46 +2,43 @@ package com.example.car4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
-        Log.d("TAG","Activity2 On Create");
-        Intent intent = new Intent(this,Activity3.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_3);
+        Log.d("TAG","Activity3 On Create");
     }
 
     @Override
     protected void onStart()
     {
         super.onStart();
-        Log.d("TAG","Activity2 On Start");
+        Log.d("TAG","Activity3 On Start");
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
-        Log.d("TAG","Activity2 On Resume");
+        Log.d("TAG","Activity3 On Resume");
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
-        Log.d("TAG","Activity2 On Pause");
+        Log.d("TAG","Activity3 On Pause");
     }
 
     @Override
     protected void onStop()
     {
         super.onStop();
-        Log.d("TAG","Activity2 On Pause");
+        Log.d("TAG","Activity3 On Pause");
     }
 }
--- app/src/main/java/com/example/car4u/MainActivity.java
+++ app/src/main/java/com/example/car4u/MainActivity.java
@@ -6,15 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("NEWTAG","OFEK");
+        Log.d("TAG","MainActivity On Create");
         Intent intent = new Intent(this,Activity2.class);
         startActivity(intent);
-
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("TAG","MainActivity On Start");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("TAG","MainActivity On Resume");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("TAG","MainActivity On Pause");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("TAG","MainActivity On Pause");
+    }
+
 }
