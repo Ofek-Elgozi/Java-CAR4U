d24f3942942d032a525ae4a32ddf0b48d9ea0dde
Ofek <ofekel@ac.sce.ac.il>
2021-10-27 20:34:55 +0300

    Edited New Activity

--- app/src/main/java/com/example/car4u/MainActivity.java
+++ app/src/main/java/com/example/car4u/MainActivity.java
@@ -9,12 +9,11 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("new", "Buba");
-        Intent intent= new Intent(this, Activity2.class);
+        Log.d("NEWTAG","OFEK");
+        Intent intent = new Intent(this,Activity2.class);
         startActivity(intent);
 
     }
