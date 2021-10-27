c97483e54f10c08d2e3215e18875cd86e024b5a0
Ofek <ofekel@ac.sce.ac.il>
2021-10-27 20:03:34 +0300

    Adding New Activity.

---
+++ app/src/main/java/com/example/car4u/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.car4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("new", "Buba");
+        Intent intent= new Intent(this, Activity2.class);
+        startActivity(intent);
+
+    }
+}
