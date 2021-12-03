be38a057b90ed8f9a0570a58a67fc51053228f0a
Ofek <ofekel@ac.sce.ac.il>
2021-12-03 17:50:48 +0200

    Updating nat_graph Build.Gradel (app+project)

--- app/src/main/java/com/example/car4u/MainActivity.java
+++ app/src/main/java/com/example/car4u/MainActivity.java
@@ -3,21 +3,23 @@ package com.example.car4u;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity
 {
+    NavController navCtrl;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirstFragment frag = new FirstFragment();
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction tran = manager.beginTransaction();
-        tran.replace(R.id.mainactivity_container,frag);
-        tran.commit();
+        NavHostFragment nav_host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_nav_host);
+        navCtrl = nav_host.getNavController();
+        NavigationUI.setupActionBarWithNavController(this,navCtrl);
     }
 }
