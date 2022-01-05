eb0fe18a42216073ff29d162d5efbca9d5f5e37a
Ofek <ofekel@ac.sce.ac.il>
2022-01-05 12:13:23 +0200

    Disabled The Back Button Of Phone

--- app/src/main/java/com/example/car4u/MainActivity.java
+++ app/src/main/java/com/example/car4u/MainActivity.java
@@ -26,4 +26,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onBackPressed()
+    {
+
+    }
 }
